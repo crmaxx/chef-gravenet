cbcaa9130b760c73c26b605c1bb03851340408df
Maxim Zhukov <crmaxx@gmail.com>
2013-05-02 04:15:25 +0400

    Zabbix was installed

--- cookbooks/main/recipes/default.rb
+++ cookbooks/main/recipes/default.rb
@@ -1,8 +1,16 @@
 package "mc"
+package "lynx"
 package "vim"
 package "git-core"
 package "localepurge"
 
+include_recipe "apache2"
+include_recipe "mysql::client"
+include_recipe "mysql::server"
+include_recipe "php"
+include_recipe "php::module_mysql"
+include_recipe "apache2::mod_php5"
+include_recipe "apache2::mod_ssl"
 include_recipe "logrotate"
 include_recipe "zabbix"
 
@@ -12,3 +20,27 @@ user node[:user][:name] do
   home "/home/#{node[:user][:name]}"
   supports manage_home: true
 end
+
+hostname = node[:hostname]
+
+file '/etc/hostname' do
+  content "#{hostname}\n"
+end
+
+service 'hostname' do
+  action :restart
+end
+
+service 'mysql' do
+  action :restart
+end
+
+file '/etc/hosts' do
+  content "127.0.0.1 localhost #{hostname}\n"
+end
+
+apache_site "default" do
+  enable false
+end
+
+node.set['zabbix']['web']['fqdn'] = hostname
